2b7aee4ab2834aa9f79c57340acf0a24a535886d
dev1d123 <dhuamanio@unsa.edu.pe>
2026-01-30 11:57:12 -0500

    feat: add history

--- hall-of-mask/src/GUI/menu.gd
+++ hall-of-mask/src/GUI/menu.gd
@@ -4,13 +4,12 @@ extends Control
 @onready var exit_button: TextureButton = $exitButton
 @onready var music: AudioStreamPlayer = $AudioStreamPlayer
 
-const LOBBY_SCENE := "res://src/levels/lobby/Lobby.tscn"
+const CINEMATIC_SCENE := "res://src/GUI/Cinematic.tscn"
 const FADE_TIME := 1.0
 
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_pressed)
 	exit_button.pressed.connect(_on_exit_pressed)
-
 	music.play()
 
 func _on_start_pressed() -> void:
@@ -21,14 +20,14 @@ func _on_start_pressed() -> void:
 	tween.tween_property(
 		music,
 		"volume_db",
-		-80.0,        
+		-80.0,
 		FADE_TIME
 	)
-	tween.tween_callback(_change_to_lobby)
+	tween.tween_callback(_change_to_cinematic)
 
-func _change_to_lobby() -> void:
+func _change_to_cinematic() -> void:
 	music.stop()
-	get_tree().change_scene_to_file(LOBBY_SCENE)
+	get_tree().change_scene_to_file(CINEMATIC_SCENE)
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
